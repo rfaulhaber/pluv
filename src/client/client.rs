f5d97780427fe9e55bc9734cf678caba456ae840
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-02-09 15:24:57 -0500

    rustfmt

--- src/client/client.rs
+++ src/client/client.rs
@@ -3,37 +3,37 @@ use std::error;
 use std::fmt;
 
 pub enum ClientType {
-	DarkSky { api_key: String },
+    DarkSky { api_key: String },
 }
 
 pub enum ForecastClient {
-	DarkSkyClient(DarkSkyClient),
+    DarkSkyClient(DarkSkyClient),
 }
 
 impl ForecastClient {
-	pub fn get_current_weather(&mut self, latitude: f64, longitude: f64) -> Forecast {
-		match self {
-			ForecastClient::DarkSkyClient(ds) => ds.get_current_weather(latitude, longitude),
-		}
-	}
+    pub fn get_current_weather(&mut self, latitude: f64, longitude: f64) -> Forecast {
+        match self {
+            ForecastClient::DarkSkyClient(ds) => ds.get_current_weather(latitude, longitude),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Forecast {
-	pub current_temp: String,
+    pub current_temp: String,
 }
 
 // TODO implement better display!
 impl std::fmt::Display for Forecast {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "current temperature: {}", self.current_temp)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "current temperature: {}", self.current_temp)
+    }
 }
 
 pub fn new_client(clientType: ClientType) -> ForecastClient {
-	match clientType {
-		ClientType::DarkSky { api_key: key } => {
-			ForecastClient::DarkSkyClient(DarkSkyClient::new(key))
-		}
-	}
+    match clientType {
+        ClientType::DarkSky { api_key: key } => {
+            ForecastClient::DarkSkyClient(DarkSkyClient::new(key))
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,61 +16,61 @@ use client::client::{ClientType, Forecast, ForecastClient};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Config {
-	service: String,
-	api_key: String,
-	latitude: String,
-	longitude: String,
+    service: String,
+    api_key: String,
+    latitude: String,
+    longitude: String,
 }
 
 fn main() {
-	let home_path = match home_dir() {
-		Some(path) => path.to_str().unwrap().to_owned(),
-		None => fatalln("could not find home directory"),
-	};
+    let home_path = match home_dir() {
+        Some(path) => path.to_str().unwrap().to_owned(),
+        None => fatalln("could not find home directory"),
+    };
 
-	let default_config_path = Path::new(home_path.as_str()).join(".pluvrc");
+    let default_config_path = Path::new(home_path.as_str()).join(".pluvrc");
 
-	let matches = App::new("pluv")
-		.version("0.1.0")
-		.about("weather app for the terminal")
-		.arg(
-			Arg::with_name("config")
-				.short("c")
-				.long("config")
-				.value_name("filepath")
-				.help("path to config file")
-				.default_value(default_config_path.to_str().unwrap()),
-		)
-		.get_matches();
+    let matches = App::new("pluv")
+        .version("0.1.0")
+        .about("weather app for the terminal")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("filepath")
+                .help("path to config file")
+                .default_value(default_config_path.to_str().unwrap()),
+        )
+        .get_matches();
 
-	let file = match File::open(&default_config_path) {
-		Ok(file) => file,
-		Err(why) => {
-			fatalln(format!("could not open {}: {}", default_config_path.display(), why).as_str());
-		}
-	};
+    let file = match File::open(&default_config_path) {
+        Ok(file) => file,
+        Err(why) => {
+            fatalln(format!("could not open {}: {}", default_config_path.display(), why).as_str());
+        }
+    };
 
-	let config: Config = match serde_yaml::from_reader(&file) {
-		Ok(result) => result,
-		Err(err) => fatalln(format!("something went wrong: {}", err).as_str()),
-	};
+    let config: Config = match serde_yaml::from_reader(&file) {
+        Ok(result) => result,
+        Err(err) => fatalln(format!("something went wrong: {}", err).as_str()),
+    };
 
-	let client_type = match config.service.as_str() {
-		"DarkSky" => ClientType::DarkSky {
-			api_key: config.api_key,
-		},
-		_ => panic!("invalid service type"),
-	};
+    let client_type = match config.service.as_str() {
+        "DarkSky" => ClientType::DarkSky {
+            api_key: config.api_key,
+        },
+        _ => panic!("invalid service type"),
+    };
 
-	let mut service = client::client::new_client(client_type);
+    let mut service = client::client::new_client(client_type);
 
-	println!(
-		"{}",
-		service.get_current_weather(
-			config.latitude.parse().unwrap(),
-			config.longitude.parse().unwrap()
-		)
-	);
+    println!(
+        "{}",
+        service.get_current_weather(
+            config.latitude.parse().unwrap(),
+            config.longitude.parse().unwrap()
+        )
+    );
 }
 
 // fn get_current_weather(service: ForecastService) -> String {
@@ -78,6 +78,6 @@ fn main() {
 // }
 
 fn fatalln(message: &str) -> ! {
-	eprintln!("{}", message);
-	process::exit(1);
+    eprintln!("{}", message);
+    process::exit(1);
 }
